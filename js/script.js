a10cb0dfbcfdf5f01b0e5db7d189775124ebe600
FraRussotto <russottofrancesco1@gmail.com>
2023-09-22 15:37:41 +0200

    [edit] data transform in array of object

--- js/script.js
+++ js/script.js
@@ -8,13 +8,28 @@
 
 //1.
 const archive = [
-  'img/01.webp',
-  'img/02.webp',
-  'img/03.webp',
-  'img/04.webp',
-  'img/05.webp'
+  {
+      image: 'img/01.webp',
+      title: 'Marvel\'s Spiderman Miles Morale',
+      text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+  }, {
+      image: 'img/02.webp',
+      title: 'Ratchet & Clank: Rift Apart',
+      text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+  }, {
+      image: 'img/03.webp',
+      title: 'Fortnite',
+      text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+  }, {
+      image: 'img/04.webp',
+      title: 'Stray',
+      text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+  }, {
+      image: 'img/05.webp',
+      title: "Marvel's Avengers",
+      text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
+  }
 ];
-
 console.log(archive)
 
 //2.
@@ -26,13 +41,12 @@ const btnDown = document.querySelector('.down');
 let counterImg = 0;
 
 //3.
-for(let i = 0; i < archive.length; i++){
-  const image = archive[i];
-  imageWrapper.innerHTML += `<div class="full hide">
-  <img src="${image}"></div>`
+archive.forEach((game) =>{
+    imageWrapper.innerHTML += `<div class="full hide">
+    <img src="${game.image}"></div>`
   miniWrapper.innerHTML += `<div class="thumbnail">
-  <img src="${image}"></div>`
-}
+  <img src="${game.image}"></div>`
+})
 
 const fullCollection = document.getElementsByClassName('full');
 fullCollection[counterImg].classList.remove('hide')
